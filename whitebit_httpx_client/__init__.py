78906d78a8531ef42ea593d4e5d1c1fd857508ce
Oleksandr Prudnikov <67575598+Grommash9@users.noreply.github.com>
2024-11-05 08:47:18 +0000

    Nonce hot fix (make it not so small) (#3)
    * nonce hot fix (make it not so small)
    
    * remove codebase

--- whitebit_httpx_client/__init__.py
+++ whitebit_httpx_client/__init__.py
@@ -32,7 +32,7 @@ class WhiteBITClient:
     def _prepare_request_data(self, request_path: str, **kwargs) -> Dict[str, Any]:
         data = {
             "request": request_path,
-            "nonce": time.time_ns(),
+            "nonce": time.time_ns() / 1_000_000,
             "nonceWindow": True,
         }
         data.update(kwargs)
